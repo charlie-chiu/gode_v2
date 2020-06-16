7b9d22ab08e5eeb47dd74bacf7c87a7070552712
Charlie_Chiu <marmot00@gmail.com>
2020-06-16 15:27:13 +0800

    refactor: extract handler

--- server.go
+++ server.go
@@ -14,19 +14,26 @@ func NewServer() (s *Server) {
 	s = &Server{}
 
 	router := http.NewServeMux()
-	router.Handle("/echo", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ws := newWSServer(w, r)
-		_, p, _ := ws.ReadMessage()
-		ws.writeBinaryMsg(p)
-	}))
 
-	router.Handle("/casino/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		gameType := strings.TrimLeft(r.URL.Path, "/casino/")
+	// for dev purpose: echo anything
+	router.Handle("/echo", http.HandlerFunc(echoHandler))
 
-		fmt.Fprint(w, gameType)
-	}))
+	// handle game process
+	router.Handle("/casino/", http.HandlerFunc(gameHandler))
 
 	s.Handler = router
 
 	return
+}
+
+func gameHandler(w http.ResponseWriter, r *http.Request) {
+	gameType := strings.TrimLeft(r.URL.Path, "/casino/")
+
+	fmt.Fprint(w, gameType)
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	ws := newWSServer(w, r)
+	_, p, _ := ws.ReadMessage()
+	ws.writeBinaryMsg(p)
 }
