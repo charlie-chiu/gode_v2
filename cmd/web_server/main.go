e56a9fde600143cbb4d092d99ee6573a4051774c
Charlie_Chiu <marmot00@gmail.com>
2020-07-01 14:03:49 +0800

    update main/web_server

--- cmd/web_server/main.go
+++ cmd/web_server/main.go
@@ -5,19 +5,13 @@ import (
 	"net/http"
 
 	"gode"
-	"gode/types"
+	"gode/casinoapi"
 )
 
 func main() {
 	hub := gode.NewHub()
-	caller := &StubAPI{}
+	caller := casinoapi.NewFlash2db("http://103.241.238.74/")
 	server := gode.NewServer(hub, caller)
 
 	log.Fatal(http.ListenAndServe(":80", server))
-}
-
-type StubAPI struct{}
-
-func (StubAPI) Call(service types.GameType, functionName string, parameters ...interface{}) ([]byte, error) {
-	panic("implement me")
 }
