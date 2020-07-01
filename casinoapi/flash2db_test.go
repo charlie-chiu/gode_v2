3d71a3f4e40e4b23f3c6a2dd54948af1e181f8d1
Charlie_Chiu <marmot00@gmail.com>
2020-07-01 11:40:11 +0800

    update flash2db_test.go

--- casinoapi/flash2db_test.go
+++ casinoapi/flash2db_test.go
@@ -6,26 +6,30 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"gode/types"
 )
 
 func TestFlash2db_Call(t *testing.T) {
 	t.Run("get correct url and return", func(t *testing.T) {
 		service := "casino.slot.line243.BuBuGaoSheng"
 		function := "beginGame"
-		param1 := "19870604xi"
-		param2 := "7788"
+		sid := types.SessionID(`19870604xi`)
+		uid := types.UserID(9527)
+		betInfo := types.BetInfo(`{"BetLevel":5}`)
+		credit := types.Credit(50000)
 		APIResult := `{"event": true}`
 
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// "/amfphp/json.php/casino.slot.line243.BuBuGaoSheng.beginGame/19870604xi/7788"
-			want := fmt.Sprintf("%s/%s.%s/%s/%s", urlPrefix, service, function, param1, param2)
+			// "/amfphp/json.php/casino.slot.line243.BuBuGaoSheng.beginGame/19870604xi/9527/{"BetLevel":5}/50000"
+			want := fmt.Sprintf("%s/%s.%s/%s/%d/%s/%d", urlPrefix, service, function, sid, uid, betInfo, credit)
 			assertURLEqual(t, r.URL.Path, want)
 
 			_, _ = fmt.Fprint(w, APIResult)
 		}))
 
 		f := NewFlash2db(server.URL)
-		gotResult, _ := f.Call(service, function, param1, param2)
+		gotResult, _ := f.Call(service, function, sid, uid, betInfo, credit)
 
 		if bytes.Compare([]byte(APIResult), gotResult) != 0 {
 			t.Errorf("want %s, got %s", APIResult, gotResult)
